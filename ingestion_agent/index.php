08000814a139a9060eacbb9268c4682be5473be9
Jacob Bexten <jacob.bexten@umontana.edu>
2022-07-11 12:02:50 -0600

    accepts incoming http request and pushes postback to redis

--- ingestion_agent/index.php
+++ ingestion_agent/index.php
@@ -1,13 +1,25 @@
 <?php
     $redis = new Redis();
 
-    try {
-        $redis->connect('127.0.0.1', 6379);
-    } catch (RedisException $exception) {
-        print_r($exception);
-    }
+    $redis->connect('127.0.0.1', 6379);
 
-    echo "Connection to server successful. ";
-    echo "Server is running: ".$redis->ping();
-    
+    echo "Connection to server successful\n";
+    echo "Server is running: ".$redis->ping()."\n";
+
+    // reads and stores incoming http POST request
+    $json = file_get_contents("php://input");
+    $json_arr = json_decode($json, true);
+
+    if ($json_arr['endpoint'] && ['data']) {
+        $postback = json_encode($json_arr);
+        $push = $redis->rPush('data', $postback);
+        if ($push) {
+            echo "Pushed successfully\n";
+        } else {
+            print_r("Error pushing to Redis\n");
+        }
+    }
+    else {
+        echo "No request found\n";
+    }
 ?>
